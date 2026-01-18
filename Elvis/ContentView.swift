6cc7c80bd2be0a3f6e6f99f2e7dce7e28e7effcf
Nick Nedjat <nick.nedjat@icloud.com>
2026-01-18 11:59:53 -0500

    Commit after Challenge was completed

--- Elvis/ContentView.swift
+++ Elvis/ContentView.swift
@@ -8,12 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var Img1 = "0"
+    @State private var Txt1 = ""
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("What's So Funny\n 'Bout")
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .foregroundStyle(.purple)
+
+            Spacer()
+            
+            Image(systemName: Img1)
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.black)
+                .frame(width: 300, height: 300)
+            Text(Txt1)
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .foregroundStyle(.purple)
+            
+            Spacer()
+            
+            HStack {
+                Button("Peace") {
+                    Img1 = "peacesign"
+                    Txt1 = "Peace"
+                }
+                Button("Love") {
+                    Img1 = "heart"
+                    Txt1 = "Love"
+                }
+                Button("Understanding") {
+                    Img1 = "lightbulb"
+                    Txt1 = "Understanding"
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            .tint(.purple)
         }
         .padding()
     }
