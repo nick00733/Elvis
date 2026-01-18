dfbb0b6dd88f5da4d86dfd8792a8f5d3b3947456
Nick Nedjat <nick.nedjat@icloud.com>
2026-01-18 11:17:38 -0500

    Initial Commit

---
+++ Elvis/ElvisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElvisApp.swift
+//  Elvis
+//
+//  Created by Nick Nedjat on 18.01.26.
+//
+
+import SwiftUI
+
+@main
+struct ElvisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
